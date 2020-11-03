d5d060874fd29b1c80f44f7f33c7f38b52835061
OtakuaErmo <luiz.paulus51@gmail.com>
2020-11-03 16:23:26 -0300

    User e Sugestao seeders criados, userSeeder finalizado

---
+++ database/seeds/SugestaoTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SugestaoModel;
+
+class SugestaoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SugestaoModel::create([
+            '' => '',
+            
+        ]);
+    }
+}
