d49e976906baa74d69d5b0e112266f0d419e1afa
OtakuaErmo <luiz.paulus51@gmail.com>
2020-11-03 15:53:09 -0300

    adicionando mais seeders e imagens ao projeto

--- database/seeds/PalavraTableSeeder.php
+++ database/seeds/PalavraTableSeeder.php
@@ -246,13 +246,258 @@ class PalavraTableSeeder extends Seeder
 
         //-------------------------------------------------------------------------animais 3
 
-        //---------------------------------------------------------------------------brinqueds 4
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Cachorro',
+            'imagem' => 'cachorro.png',
+            'video_src' => '-l57D634Jus',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Cavalo',
+            'imagem' => 'cavalo.png',
+            'video_src' => 'hZtNro0NsG8',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Gato',
+            'imagem' => 'gato.png',
+            'video_src' => 'A-1RPxezi40',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Jacaré',
+            'imagem' => 'jacare.png',
+            'video_src' => 'ZjoxPd-FXHs',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Macaco',
+            'imagem' => 'macaco.png',
+            'video_src' => 'IZ8Ze75rNVk',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Pássaro',
+            'imagem' => 'passaro.png',
+            'video_src' => 'mBniJAyWJ4M',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Peixe',
+            'imagem' => 'peixe.png',
+            'video_src' => '8IwZK83yWE4',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Porco',
+            'imagem' => 'porco.png',
+            'video_src' => 'Ph1yO4ALVzU',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Rato',
+            'imagem' => 'rato.png',
+            'video_src' => 'anDCmTwNa_c',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '3',
+            'palavra' => 'Sapo',
+            'imagem' => 'sapo.png',
+            'video_src' => 'ZkqYl4BQV9Q',
+        ]);
+
+        //---------------------------------------------------------------------------brinquedos 4
+
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Baralho',
+            'imagem' => 'baralho.png',
+            'video_src' => 'TRJXMoFkJQg',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Bola',
+            'imagem' => 'bola.png',
+            'video_src' => '0sePd0Ci20g',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Boneca',
+            'imagem' => 'boneca.png',
+            'video_src' => 'VCPXwMIcCGY',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Carrinho',
+            'imagem' => 'carrinho.png',
+            'video_src' => '5niRsxryPcs',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Damas',
+            'imagem' => 'damas.png',
+            'video_src' => 'wrLAmxP6Piw',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Ioiô',
+            'imagem' => 'ioio.png',
+            'video_src' => '9KG0RbhVaNA',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Urso de Pelúcia',
+            'imagem' => 'ursodepelucia.png',
+            'video_src' => 'lVaiHdHR5BM',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Pipa',
+            'imagem' => 'pipa.png',
+            'video_src' => '9KQydVrJixk',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Quebra-Cabeça',
+            'imagem' => 'quebracabeca.png',
+            'video_src' => 'fFMAcHQSbsI',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '4',
+            'palavra' => 'Video-Game',
+            'imagem' => 'videogame.png',
+            'video_src' => 'ofuI0lsTB9k',
+        ]);
 
         //-----------------------------------------------------------------------------frutas 5
 
         //--------------------------------------------------------------------------------cores 6
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Amarelo',
+            'imagem' => 'amarelo.png',
+            'video_src' => '9zuFDzcaYrU',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Vermelho',
+            'imagem' => 'vermelho.png',
+            'video_src' => 'oFKy8NzL5fY',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Verde',
+            'imagem' => 'verde.png',
+            'video_src' => 'VxTsu5SJCwE',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Roxo',
+            'imagem' => 'roxo.png',
+            'video_src' => 'BQFwMPJtNAs',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Marrom',
+            'imagem' => 'marrom.png',
+            'video_src' => 'bPFitIcwn4k',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Azul',
+            'imagem' => 'azul.png',
+            'video_src' => 'glvCL7Ppq3Y',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Rosa',
+            'imagem' => 'rosa.png',
+            'video_src' => 'YMfOV9_RAGk',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Preto',
+            'imagem' => 'preto.png',
+            'video_src' => 'VyA0T7z6Jus',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Laranja',
+            'imagem' => 'laranja.png',
+            'video_src' => '2OooQnwxYOc',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '6',
+            'palavra' => 'Branco',
+            'imagem' => 'branco.png',
+            'video_src' => '7ly18UDkgNQ',
+        ]);
+
+
+
 
         //----------------------------------------------------------------------------------cumprimentos 7
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Bom Dia',
+            'imagem' => 'bomdia.png',
+            'video_src' => 'z8qT7XVEOYo',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Boa Tarde',
+            'imagem' => 'boatarde.png',
+            'video_src' => '1MfLaPLxJ9E',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Boa Noite',
+            'imagem' => 'boanoite.png',
+            'video_src' => 'kvXym4Zos2s',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Olá',
+            'imagem' => 'ola.png',
+            'video_src' => 'XEshn-ciHws',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Tchau',
+            'imagem' => 'tchau.png',
+            'video_src' => 'XqEXmCPSDz0',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Obrigado',
+            'imagem' => 'obrigado.png',
+            'video_src' => 'aSqkJZ9axbs',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'De Nada',
+            'imagem' => 'denada.png',
+            'video_src' => 'gR-N71aWN3I',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Tudo bem',
+            'imagem' => 'tudobem.png',
+            'video_src' => 'dC9ZbTy1Yw0',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Por Favor',
+            'imagem' => 'porfavor.png',
+            'video_src' => 'tOD9zToCUBQ',
+        ]);
+        PalavraModel::create([
+            'id_contexto' => '7',
+            'palavra' => 'Desculpa',
+            'imagem' => 'desculpa.png',
+            'video_src' => 'Lj-vRUK0BPM',
+        ]);
 
     }
 }
