c827de956fff046b1dade9e705d7b14bf49e7649
OtakuaErmo <luiz.paulus51@gmail.com>
2020-11-08 22:57:58 -0300

    diretorio das rotas organizado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,6 @@ class AuthController extends Controller
             return redirect()->route('index');
         }
         return redirect()->route('index');
-
     }
 
     public function showLoginForm()
